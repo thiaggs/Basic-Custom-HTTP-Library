533660f44490dd2e7364a80cae45f7ee1e6d743e
Thiago Tardin <38774020+thiaggs@users.noreply.github.com>
2020-02-10 14:45:50 -0300

    Add files via upload

---
+++ easyHttpAwait/app.js
@@ -0,0 +1,27 @@
+const http = new EasyHTTP;
+
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+const userData = {
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+    adress:{
+        street: 'Kulas light',
+        suite: 'apt. 556'
+    }
+};
+// http.post('https://jsonplaceholder.typicode.com/users', userData)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// http.put('https://jsonplaceholder.typicode.com/users/2', userData)
+    // .then(data => console.log(data))
+    // .catch(err => console.log(err));
+
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+
